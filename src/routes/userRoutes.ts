d0253c3e7965e2ccad3ba04c1291f567e8c21add
Klevisson Weskley <122038836+KlevissonWeskley@users.noreply.github.com>
2025-06-18 00:30:22 -0300

    fix: authenticate

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { authenticate } from "../controllers/user/authenticate";
 import { getAllUsers } from "../controllers/user/getAllUsers";
 import { registerUser } from "../controllers/user/register";
 import { getUserByEmail } from "../controllers/user/getUserByEmail";
 
 export async function userRoutes(app: FastifyInstance) {
     app.post('/users/register', registerUser)
-    app.post('/auth/google/callback', authenticate)
     app.get('/users/getAll', getAllUsers)
     app.get('/users/by-email/:email', getUserByEmail)
 }
